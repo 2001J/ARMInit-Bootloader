a5a35ccf5eeba5c7779afc79da26c2274840d82d
2001J <josejeth26@gmail.com>
2024-11-18 16:02:55 +0100

    feat: Added bootloader info display command
    - Implemented print_bootloader_info() function
    - Added version, build date and time constants
    - Displays target hardware information
    - Accessible via 'i' command in bootloader CLI

--- bootloader.c
+++ bootloader.c
@@ -1,6 +1,23 @@
 #include "uart.h"
 #include "system.h"
 
+#define BOOTLOADER_VERSION "1.0.0"
+#define BUILD_DATE __DATE__
+#define BUILD_TIME __TIME__
+
+
+void print_bootloader_info(void) {
+    uart_puts("\r\n=== Bootloader Info ===\r\n");
+    uart_puts("Version: ");
+    uart_puts(BOOTLOADER_VERSION);
+    uart_puts("\r\nBuild: ");
+    uart_puts(BUILD_DATE);
+    uart_puts(" ");
+    uart_puts(BUILD_TIME);
+    uart_puts("\r\nTarget: LM3S6965\r\n");
+    uart_puts("===================\r\n");
+}
+
 int main(void) {
     uart_init();
     
@@ -15,6 +32,9 @@ int main(void) {
     while(1) {
         char cmd = uart_getc();
         switch(cmd) {
+            case 'i':
+                print_bootloader_info();
+                break;
             case 'r':
                 uart_puts("Resetting...\r\n");
                 system_reset();
